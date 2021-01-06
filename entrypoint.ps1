9d0784867408ce8b681b86204e1d8c54e1b9281e
Justin Yoo <justin.yoojh@gmail.com>
2021-01-06 16:12:05 +0900

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -7,29 +7,17 @@ Param(
 # Install PowerShell-Yaml module
 Install-Module -Name powershell-yaml -Scope AllUsers -AllowClobber -Force
 
-$Markdown
-
 # Extract frontmatter
 $md = $Markdown.Trim('"').Replace("\\r", "`r").Replace("\r", "`r").Replace("\\n", "`n").Replace("\n", "`n").Trim() -split ("---")
 
 if ($md[0] -eq "") {
-    Write-Output "md[1]: "
-    $md[1]
     $yaml = $md[1].Trim()
 } else {
-    Write-Output "md[0]: "
-    $md[0]
     $yaml = $md[0].Trim()
 }
 
-Write-Output "YAML:"
-$yaml
-
 # Convert frontmatter YAML to JSON
 $deserialised = $yaml | ConvertFrom-Yaml
-
-$deserialised
-
 $jsonised = $deserialised | ConvertTo-Json -Compress
 
 $jsonised
